f0d64aab60cfd27a448c9b847b66ffe36d0a6ab3
Basil Sut <1vasilisk12@gmail.com>
2021-09-14 05:28:49 +0300

    CMake3

---
+++ Tokenizer.cpp
@@ -0,0 +1,14 @@
+#include "Tokenizer.hpp"
+
+namespace simpleparser{
+    using namespace std;
+    vector<Token> Tokenizer::parse(const string &inProgram);
+    vector<Token> tokens;
+    Token currentToken;
+    for(currCh : inProgram){
+        switch(currCh){
+            case '0':
+            case '1':
+        }
+    }
+}
